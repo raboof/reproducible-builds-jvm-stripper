a7ffc20480365855aec33e73d2f11669fcf60aeb
Arnout Engelen <arnout@bzzt.net>
2024-02-18 12:50:37 +0100

    No integration tests
    as we're not a maven plugin but a regular library

--- src/it/extra-manifest-attributes/verify.groovy
+++
@@ -1,6 +0,0 @@
-def zip = new java.util.zip.ZipFile(new File(basedir, "target/extra-manifest-attributes-1.0-SNAPSHOT.jar"))
-def manifest = null
-zip.entries().findAll{ it.name == "META-INF/MANIFEST.MF" }
-    .each{ manifest = zip.getInputStream(it).text }
-assert manifest.trim().isEmpty()
-
